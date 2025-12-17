e9c1a70392376217df2ae879aa8a2f3a57779ed1
teunrompa <teunrompanew@gmail.com>
2025-12-17 21:09:07 +0100

    added helper methods

--- src/mixer.rs
+++ src/mixer.rs
@@ -1,20 +1,26 @@
-use crate::audio_source::AudioSource;
+use crate::{audio_source::AudioSource, waves::SineWave};
 
 pub struct Mixer {
     sources: Vec<Box<dyn AudioSource>>,
     volume: f32,
+    sample_rate: f32,
 }
 
 impl Mixer {
-    pub fn new(volume: f32) -> Self {
+    pub fn new(volume: f32, sample_rate: f32) -> Self {
         Self {
             sources: Vec::new(),
             volume,
+            sample_rate,
         }
     }
 
     pub fn add_source(&mut self, source: Box<dyn AudioSource>) {
         self.sources.push(source);
+    }
+
+    pub fn add_sources(&mut self, sources: Vec<Box<dyn AudioSource>>) {
+        self.sources.extend(sources);
     }
 
     pub fn next_sample(&mut self) -> f32 {
@@ -23,5 +29,16 @@ impl Mixer {
         let mixed: f32 = self.sources.iter_mut().map(|s| s.next_sample()).sum();
 
         (mixed * self.volume).clamp(-1.0, 1.0)
+    }
+
+    //Helper methods
+    pub fn add_sine_wave(&mut self, frequency: f32) {
+        self.add_source(Box::new(SineWave::new(frequency, self.sample_rate)));
+    }
+
+    pub fn play_chord(&mut self, frequencies: &[f32]) {
+        for &freq in frequencies {
+            self.add_sine_wave(freq);
+        }
     }
 }
